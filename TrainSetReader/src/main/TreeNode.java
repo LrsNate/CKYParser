8ee39dbced797726c62d01e12946d582c350bd39
kirkira <Kira@172.28.130.193>
2014-06-21 15:11:08 +0200

    Added .jar files

--- TrainSetReader/src/main/TreeNode.java
+++ TrainSetReader/src/main/TreeNode.java
@@ -49,7 +49,7 @@ public class TreeNode
 			this._value = new Symbol(tab[0]);
 			this._children = new LinkedList<TreeNode>();
 			if (tab.length > 1) {
-				// ??? this is the lexical entry...
+				// this is the lexical entry
 				// this._children.addLast(new TreeNode(tab[1])); 
 				// new Symbol(value = tab[1], is_terminal = true)
 				this._children.addLast(new TreeNode(new Symbol(tab[1], true))); 
