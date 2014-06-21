9f10763e38a1f4e9b882bd9b8db2d652dc14e3c9
kirkira <Kira@172.28.130.193>
2014-06-21 16:40:21 +0200

    Added option to print probabilities or not, added categories to the bare phrase.

--- TrainSetReader/src/test/EliminateNodesTest.java
+++ TrainSetReader/src/test/EliminateNodesTest.java
@@ -1,14 +1,18 @@
 package test;
 
+import main.Environment;
 import main.TreeNode;
 import main.IsAlien;
 
 public class EliminateNodesTest {
 
 	public static void main(String[] args) {
+		Environment.setLexical(true);
 		String s1 = "(S (Z7 (Z6 (R r) (Z1 x)) (A (B (Z4 (C c))) (U u))))";
 		//String s1 = "(S (Z7 (Z6 (R r) (Z1 (A a))) (Ziht8 (B (Z4 (C c))) (U u))))";
 		TreeNode input = new TreeNode(s1);
+		System.out.println("Phrase : " + s1);
+		System.out.println("Bare phrase : " + input.getBarePhrase());
 		System.out.println("Before transform:");
 		System.out.println(input.dump());
 		// define the set of nodes to be deleted
