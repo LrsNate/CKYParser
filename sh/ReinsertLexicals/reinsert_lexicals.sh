3dfab66f2e7465264eb5753937dd9e46d31a53a5
yu-li-ya <yuliya@calculate.local>
2014-06-24 00:07:35 +0200

    UNK + NULL dans l'output

--- sh/ReinsertLexicals/reinsert_lexicals.sh
+++ sh/ReinsertLexicals/reinsert_lexicals.sh
@@ -14,6 +14,7 @@ function is_terminal(s) {
  return ((i % 2) != 0);
 }
 function get_lexical_terminal() {
+ print "( " word_number, nwords " )";
  if (word_number > nwords) {
    print "ERROR: length mismatch between the non-lexical parse and the tagged phrase";
    exit 1;
@@ -29,22 +30,29 @@ function get_lexical_terminal() {
  return lex;
 }
 {
-  str_parse = $0;
+  delete tagged_phrase; delete parse;
+  numero_de_la_phrase = $1; print "field 1 = " $1;
+  gsub("*", "", numero_de_la_phrase);
+  print "numero = " numero_de_la_phrase;
+  ## get the line with the parse
+  getline; str_parse = $0; print "str_parse = " $0;
   if (!((getline str_tagged_phrase < f_tagged_phrases) > 0)) {
         print "ERROR: Not enough lines in the file with tagged phrases.";
   	exit 1;
   }
+  if (str_parse == "(NULL null)") { print str_parse; next; }
   nwords = split(str_tagged_phrase, tagged_phrase, " "); 
   n_items_in_parse = split(str_parse, parse, " ");
   res = ""; k = 1; word_number = 0;
   while (k < n_items_in_parse+1) {
-    parse_item = parse[k];
-    if (!(is_terminal(parse_item))) {
-      res = res " " parse_item;
-    } else {
-      word_number++;
-      res = res " " get_lexical_terminal();
-    }
+      parse_item = parse[k];
+      if (!(is_terminal(parse_item))) {
+        res = res " " parse_item;
+      } else {
+        print "is_terminal = " parse_item; 
+        word_number++;
+        res = res " " get_lexical_terminal();
+      }
     k++;
   }
   print res;
