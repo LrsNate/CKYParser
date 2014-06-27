485802ee2bfc6acd75b4e348f056cb6cf184196f
yu-li-ya <yuliya@calculate.local>
2014-06-27 02:33:13 +0200

    Parsing, tagging gold, labeled evaluation

---
+++ Eval/EVALB/run_evalb_labeled.sh
@@ -0,0 +1,12 @@
+
+set -o nounset
+#/////////////////////////////
+f_gold="$1"
+f_test="$2"
+# ////////////////////////////
+dir=`dirname $0`
+EVALB="$dir""/evalb"
+f_evalb_parameters="$dir""/evalb_param_labeled.prm"
+
+"$EVALB" -p "$f_evalb_parameters" "$f_gold" "$f_test"
+
